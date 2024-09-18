f18dba2c0c7782de8e82528c936874d61830161e
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-18 17:08:07 -0400

    Updated components, data, and css code

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -12,6 +12,7 @@ function Pet({pet}){
         <li className="pet">
             <img src={pet.image} alt={pet.name}/>
             <h4 onClick={toggleDisplayAnimalType} className={displayAnimalType ? "display-animal-type" : ""}>{displayAnimalType ? pet.animal_type : pet.name}</h4>
+            <button className="like-button">{/* pet's likes goes here */} Likes</button>
             <button className="adopt-button">Adopt</button>
         </li>
     );
--- src/data/pets.js
+++ src/data/pets.js
@@ -3,43 +3,50 @@ const pets = [
         id: 1,
         name: "Fido",
         image: "./images/dog.jpeg",
-        animal_type: "Dog"
+        animal_type: "Dog",
+        likes: 5
     },
     {
         id: 2,
         name: "Kitty",
         image: "./images/cat.png",
-        animal_type: "Cat"
+        animal_type: "Cat",
+        likes: 9
     },
     {
         id: 3,
         name: "Hammy",
         image: "./images/hamster.jpeg",
-        animal_type: "Hamster"
+        animal_type: "Hamster",
+        likes: 8
     },
     {
         id: 4,
         name: "Polly",
         image: "./images/parrot.jpeg",
-        animal_type: "Bird"
+        animal_type: "Bird",
+        likes: 4
     },
     {
         id: 5,
         name: "Angel",
         image: "./images/horse.jpeg",
-        animal_type: "Horse"
+        animal_type: "Horse",
+        likes: 9
     },
     {
         id: 6,
         name: "Fluffy",
         image: "./images/guinea-pig.jpeg",
-        animal_type: "Guinea Pig"
+        animal_type: "Guinea Pig",
+        likes: 1
     },
     {
         id: 7,
         name: "Goldie",
         image: "./images/fish.jpeg",
-        animal_type: "Fish"
+        animal_type: "Fish",
+        likes: 4
     }
 ]
 

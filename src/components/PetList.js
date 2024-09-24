62cd2bb44a735315346f74efa008a1363f3f6fb8
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-24 17:59:20 -0400

    Update PetList.js

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -6,8 +6,7 @@ function PetList({ pets, deletePet, updatePet }){
     // Write the code to retrieve the pets stateful variable using useOutletContext()
 
     const petComponents = pets.map(pet => {
-        // Delete the deletePet and updatePet props from <Pet/>
-        return <Pet key={pet.id} pet={pet} deletePet={deletePet} updatePet={updatePet}/>
+        return <Pet key={pet.id} pet={pet}/>
     })
 
     return (

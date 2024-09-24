298b14a3e456034de55338ae92dc2ef4d7a684a5
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-24 17:57:56 -0400

    Update Pet.js

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -12,6 +12,7 @@ function Pet({pet}){
         <li className="pet">
             <img src={pet.image} alt={pet.name}/>
             <h4 onClick={toggleDisplayAnimalType} className={displayAnimalType ? "display-animal-type" : ""}>{displayAnimalType ? pet.animal_type : pet.name}</h4>
+            {/* Render a Link component here that has a "to" prop that will allow you to visit a "/pets/:id" route, where id is replaced with the pet's id (i.e.: Using pet.id). The text content should have the following format, using string interpolation: "View ${pet.name}'s profile"*/}
         </li>
     );
 }

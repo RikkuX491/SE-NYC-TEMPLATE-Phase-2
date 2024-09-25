1fe88c127e12d319d297d7ec788f1192faa04db5
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-25 11:24:05 -0400

    Updated starter code

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,6 @@ function App(){
 
     const [pets, setPets] = useState([]);
 
-    // const [route, setRoute] = useState(window.location.pathname);
-
-    // let component = null;
-    // if(route === "/"){
-    //   component = <PetList pets={pets} deletePet={deletePet} updatePet={updatePet}/>
-    // }
-    // else if(route === "/add_pet"){
-    //   component = <NewPetForm addPet={addPet}/>
-    // }
-
     useEffect(() => {
         fetch('http://localhost:4000/pets')
         .then(response => response.json())
@@ -71,22 +61,11 @@ function App(){
         })))
     }
 
-    // function navigate(event){
-    //   event.preventDefault();
-    //   window.history.pushState(null, "", event.target.href);
-    //   setRoute(window.location.pathname);
-    // }
-
     return (
       <div className="app">
-        {/* <nav className="navbar">
-          <a className={route === "/" ? "active" : ""} onClick={(event) => navigate(event, "/")} href="/">Home</a>
-          <a className={route === "/add_pet" ? "active" : ""} onClick={(event) => navigate(event, "/add_pet")} href="/add_pet">Add Pet</a>
-        </nav> */}
         <Header/>
         <NewPetForm addPet={addPet}/>
         <PetList pets={pets}/>
-        {/* {component} */}
       </div>
     );
 }
